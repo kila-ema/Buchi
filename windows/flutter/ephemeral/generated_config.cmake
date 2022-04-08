ec7fa878a78d44f615b4b2571514b6c0a4b4424f
kila-ema <85491322+kila-ema@users.noreply.github.com>
2022-04-08 15:19:09 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\emana\\Downloads\\flutter_windows_2.2.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\buchi" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\emana\\Downloads\\flutter_windows_2.2.3-stable\\flutter"
+  "PROJECT_DIR=C:\\buchi"
+  "FLUTTER_ROOT=C:\\Users\\emana\\Downloads\\flutter_windows_2.2.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\buchi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\buchi"
+  "FLUTTER_TARGET=C:\\buchi\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\buchi\\.dart_tool\\package_config.json"
+)
